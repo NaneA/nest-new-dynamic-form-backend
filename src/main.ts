9e26141d9e4e1a79c533b38368b9006c25e549fd
Nane Abrahamyan <nane.abrahamyan@blockstars.tech>
2025-06-23 01:19:33 +0400

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5175',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
