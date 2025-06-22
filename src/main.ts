0d2284b6f1e51e8f1d54e80e8458832c3065bc23
Nane Abrahamyan <nane.abrahamyan@blockstars.tech>
2025-06-23 02:38:00 +0400

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5175',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
